6d57f6a0bb561db02b4b520a1a09f88236730f02
themurgsguy <ernest.da.cruz@gmail.com>
2019-10-05 15:32:30 +0100

    Added firing mechanic to ship

---
+++ scenes/Asteroid.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+func _process(delta):
+	pass
+
+
+func _on_Asteroid_area_entered(area):
+	pass # Replace with function body.
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -4,6 +4,9 @@ const MOVE_SPEED = 150.0
 const SCREEN_WIDTH = 320
 const SCREEN_HEIGHT = 180
 
+var shot_scene = preload("res://scenes/Shot.tscn")
+var can_shoot = true
+
 
 func _process(delta):
 	var input_dir = Vector2()
@@ -28,4 +31,23 @@ func _process(delta):
 		position.y = 0.0
 	elif position.y > SCREEN_HEIGHT:
 		position.y = SCREEN_HEIGHT
+	
+	if Input.is_key_pressed(KEY_SPACE) and can_shoot:
+		can_shoot = false
+		get_node("ReloadTimer").start()
+		
+		var stage_node = get_parent()
+		
+		var left_shot_instance = shot_scene.instance()
+		left_shot_instance.position = position + Vector2(9, -5)
+		
+		var right_shot_instance = shot_scene.instance()
+		right_shot_instance.position = position + Vector2(9, 5)
+		
+		stage_node.add_child(left_shot_instance)
+		stage_node.add_child(right_shot_instance)
+
+
+func _on_ReloadTimer_timeout():
+	can_shoot = true
 
